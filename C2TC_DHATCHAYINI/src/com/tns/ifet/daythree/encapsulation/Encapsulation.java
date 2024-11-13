91114fc2fd26e862ae5573d5fc7eb1c39ce65038
dhatchayini123 <dhatchayini75@gmail.com>
2024-11-12 20:10:13 -0800

    commit

---
+++ C2TC_DHATCHAYINI/src/com/tns/ifet/daythree/encapsulation/Encapsulation.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.daythree.encapsulation;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OopsConcept obj=new OopsConcept();
+		obj.setSerialNum(101);
+		obj.setName("Dhatch");
+		obj.setAge(22);
+		System.out.println(obj);
+	}
+}
+// use get and set to call it
