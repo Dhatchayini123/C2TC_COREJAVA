71e5e7102e4e308fe7b2e58df98ee093fc843d1a
dhatchayini123 <dhatchayini75@gmail.com>
2024-12-13 08:00:32 -0800

    First commit

---
+++ Assignment1/src/com/tns/ifet/assignment/AssignmentMain.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.assignment;
+import com.tns.ifet.assignment.employees.Manager;
+import com.tns.ifet.assignment.employees.Developer;
+import com.tns.ifet.assignment.utilities.EmployeeUtilities;
+public class AssignmentMain {
+ public static void main(String[] args) {
+ Manager manager = new Manager("Ram", 101, 90000, "Sales");
+ Developer developer = new Developer("Somu", 102, 80000, "Java");
+ EmployeeUtilities.printEmployeeDetails(manager);
+ EmployeeUtilities.printEmployeeDetails(developer);
+ }
+}
